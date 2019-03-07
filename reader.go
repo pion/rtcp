02c8a2af0de245fc28658d3fdf2bf039771f7008
Michael MacDonald <github@macdonald.cx>
2019-03-07 14:02:29 -0500

    Revert "Rename Reader -> Decoder, return a Packet"
    This reverts commit a88966a47c561ea2f1003c82e54314924fa4a790.

--- reader.go
+++ reader.go
@@ -2,30 +2,29 @@ package rtcp
 
 import "io"
 
-// A Decoder reads packets from an RTCP combined packet.
-type Decoder struct {
+// A Reader reads packets from an RTCP combined packet.
+type Reader struct {
 	r io.Reader
 }
 
-// NewDecoder creates a new Decoder reading from r.
-func NewDecoder(r io.Reader) *Decoder {
-	return &Decoder{r}
+// NewReader creates a new Reader reading from r.
+func NewReader(r io.Reader) *Reader {
+	return &Reader{r}
 }
 
-// DecodePacket reads one packet from r.
+// ReadPacket reads one packet from r.
 //
 // It returns the parsed packet Header and a byte slice containing the encoded
 // packet data (including the header). How the packet data is parsed depends on
 // the Type field contained in the Header.
-func (r *Decoder) DecodePacket() (Packet, error) {
+func (r *Reader) ReadPacket() (header Header, data []byte, err error) {
 	// First grab the header
-	var header Header
 	headerBuf := make([]byte, headerLength)
 	if _, err := io.ReadFull(r.r, headerBuf); err != nil {
-		return nil, err
+		return header, data, err
 	}
 	if err := header.Unmarshal(headerBuf); err != nil {
-		return nil, err
+		return header, data, err
 	}
 
 	packetLen := (header.Length + 1) * 4
@@ -33,9 +32,10 @@ func (r *Decoder) DecodePacket() (Packet, error) {
 	// Then grab the rest
 	bodyBuf := make([]byte, packetLen-headerLength)
 	if _, err := io.ReadFull(r.r, bodyBuf); err != nil {
-		return nil, err
+		return header, data, err
 	}
-	data := append(headerBuf, bodyBuf...)
 
-	return Unmarshal(data)
+	data = append(headerBuf, bodyBuf...)
+
+	return header, data, nil
 }
--- reader_test.go
+++ reader_test.go
@@ -72,14 +72,18 @@ var realPacket = []byte{
 }
 
 func TestUnmarshal(t *testing.T) {
-	d := NewDecoder(bytes.NewReader(realPacket))
+	r := NewReader(bytes.NewReader(realPacket))
 
 	// ReceiverReport
-	packet, err := d.DecodePacket()
+	_, packet, err := r.ReadPacket()
 	if err != nil {
 		t.Fatalf("Read rr: %v", err)
 	}
-	assert.IsType(t, packet, (*ReceiverReport)(nil), "Unmarshalled to incorrect type")
+	var parsed Packet
+	if parsed, err = Unmarshal(packet); err != nil {
+		t.Errorf("Unmarshal parsed: %v", err)
+	}
+	assert.IsType(t, parsed, (*ReceiverReport)(nil), "Unmarshalled to incorrect type")
 
 	wantRR := &ReceiverReport{
 		SSRC: 0x902f9e2e,
@@ -94,16 +98,20 @@ func TestUnmarshal(t *testing.T) {
 		}},
 		ProfileExtensions: []byte{},
 	}
-	if got, want := wantRR, packet; !reflect.DeepEqual(got, want) {
+	if got, want := wantRR, parsed; !reflect.DeepEqual(got, want) {
 		t.Errorf("Unmarshal rr: got %#v, want %#v", got, want)
 	}
 
 	// SourceDescription
-	packet, err = d.DecodePacket()
+	_, packet, err = r.ReadPacket()
 	if err != nil {
 		t.Fatalf("Read sdes: %v", err)
 	}
-	assert.IsType(t, packet, (*SourceDescription)(nil), "Unmarshalled to incorrect type")
+
+	if parsed, err = Unmarshal(packet); err != nil {
+		t.Errorf("Unmarshal parsed: %v", err)
+	}
+	assert.IsType(t, parsed, (*SourceDescription)(nil), "Unmarshalled to incorrect type")
 
 	wantSdes := &SourceDescription{
 		Chunks: []SourceDescriptionChunk{
@@ -119,52 +127,66 @@ func TestUnmarshal(t *testing.T) {
 		},
 	}
 
-	if got, want := packet, wantSdes; !reflect.DeepEqual(got, want) {
+	if got, want := parsed, wantSdes; !reflect.DeepEqual(got, want) {
 		t.Errorf("Unmarshal sdes: got %#v, want %#v", got, want)
 	}
 
 	// Goodbye
-	packet, err = d.DecodePacket()
+	_, packet, err = r.ReadPacket()
 	if err != nil {
 		t.Fatalf("Read bye: %v", err)
 	}
-	assert.IsType(t, packet, (*Goodbye)(nil), "Unmarshalled to incorrect type")
+
+	if parsed, err = Unmarshal(packet); err != nil {
+		t.Errorf("Unmarshal parsed: %v", err)
+	}
+
+	assert.IsType(t, parsed, (*Goodbye)(nil), "Unmarshalled to incorrect type")
 
 	wantBye := &Goodbye{
 		Sources: []uint32{0x902f9e2e},
 	}
-	if got, want := packet, wantBye; !reflect.DeepEqual(got, want) {
+	if got, want := parsed, wantBye; !reflect.DeepEqual(got, want) {
 		t.Errorf("Unmarshal bye: got %#v, want %#v", got, want)
 	}
 
 	// PictureLossIndication
-	packet, err = d.DecodePacket()
+	_, packet, err = r.ReadPacket()
 	if err != nil {
 		t.Fatalf("Read pli: %v", err)
 	}
 
-	assert.IsType(t, packet, (*PictureLossIndication)(nil), "Unmarshalled to incorrect type")
+	if parsed, err = Unmarshal(packet); err != nil {
+		t.Errorf("Unmarshal parsed: %v", err)
+	}
+
+	assert.IsType(t, parsed, (*PictureLossIndication)(nil), "Unmarshalled to incorrect type")
 
 	wantPli := &PictureLossIndication{
 		SenderSSRC: 0x902f9e2e,
 		MediaSSRC:  0x902f9e2e,
 	}
-	if got, want := packet, wantPli; !reflect.DeepEqual(got, want) {
+	if got, want := parsed, wantPli; !reflect.DeepEqual(got, want) {
 		t.Errorf("Unmarshal pli: got %#v, want %#v", got, want)
 	}
 
 	// RapidResynchronizationRequest
-	packet, err = d.DecodePacket()
+	_, packet, err = r.ReadPacket()
 	if err != nil {
 		t.Fatalf("Read rrr: %v", err)
 	}
-	assert.IsType(t, packet, (*RapidResynchronizationRequest)(nil), "Unmarshalled to incorrect type")
+
+	if parsed, err = Unmarshal(packet); err != nil {
+		t.Errorf("Unmarshal parsed: %v", err)
+	}
+
+	assert.IsType(t, parsed, (*RapidResynchronizationRequest)(nil), "Unmarshalled to incorrect type")
 
 	wantRrr := &RapidResynchronizationRequest{
 		SenderSSRC: 0x902f9e2e,
 		MediaSSRC:  0x902f9e2e,
 	}
-	if got, want := packet, wantRrr; !reflect.DeepEqual(got, want) {
+	if got, want := parsed, wantRrr; !reflect.DeepEqual(got, want) {
 		t.Errorf("Unmarshal rrr: got %#v, want %#v", got, want)
 	}
 }
@@ -174,8 +196,8 @@ func TestReadEOF(t *testing.T) {
 		0x81, 0xc9, // missing type & len
 	}
 
-	d := NewDecoder(bytes.NewReader(shortHeader))
-	_, err := d.DecodePacket()
+	r := NewReader(bytes.NewReader(shortHeader))
+	_, _, err := r.ReadPacket()
 	if got, want := err, io.ErrUnexpectedEOF; got != want {
 		t.Fatalf("read short header: got err = %v, want %v", got, want)
 	}
