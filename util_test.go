1ef2c948f87fd0c5a811ba91c75671aff36bff58
Joe Turki <git@joeturki.com>
2025-03-29 13:31:30 +0200

    Match expected error instead of the error message

--- util_test.go
+++ util_test.go
@@ -34,42 +34,43 @@ func TestGetPadding(t *testing.T) {
 
 func TestSetNBitsOfUint16(t *testing.T) {
 	for _, test := range []struct {
-		name   string
-		source uint16
-		size   uint16
-		index  uint16
-		value  uint16
-		result uint16
-		err    string
+		name        string
+		source      uint16
+		size        uint16
+		index       uint16
+		value       uint16
+		result      uint16
+		expectedErr error
 	}{
 		{
-			"setOneBit", 0, 1, 8, 1, 128, "",
+			"setOneBit", 0, 1, 8, 1, 128, nil,
 		},
 		{
-			"setStatusVectorBit", 0, 1, 0, 1, 32768, "",
+			"setStatusVectorBit", 0, 1, 0, 1, 32768, nil,
 		},
 		{
-			"setStatusVectorSecondBit", 32768, 1, 1, 1, 49152, "",
+			"setStatusVectorSecondBit", 32768, 1, 1, 1, 49152, nil,
 		},
 		{
-			"setStatusVectorInnerBitsAndCutValue", 49152, 2, 6, 11111, 49920, "",
+			"setStatusVectorInnerBitsAndCutValue", 49152, 2, 6, 11111, 49920, nil,
 		},
 		{
-			"setRunLengthSecondTwoBit", 32768, 2, 1, 1, 40960, "",
+			"setRunLengthSecondTwoBit", 32768, 2, 1, 1, 40960, nil,
 		},
 		{
-			"setOneBitOutOfBounds", 32768, 2, 15, 1, 0, "invalid size or startIndex",
+			"setOneBitOutOfBounds", 32768, 2, 15, 1, 0, errInvalidSizeOrStartIndex,
 		},
 	} {
 		test := test
 		t.Run(test.name, func(t *testing.T) {
 			got, err := setNBitsOfUint16(test.source, test.size, test.index, test.value)
-			if err != nil {
-				assert.Equalf(t, test.err, err.Error(), "setNBitsOfUint16 %q : got = %v, want %v", test.name, err, test.err)
-
-				return
+			if test.expectedErr != nil {
+				assert.ErrorIs(t, err, test.expectedErr)
+			} else {
+				assert.NoError(t, err)
 			}
-			assert.Equalf(t, test.result, got, "setNBitsOfUint16 %q : got = %v, want %v", test.name, got, test.result)
+
+			assert.Equalf(t, test.result, got, "setNBitsOfUint16 %q", test.name)
 		})
 	}
 }
